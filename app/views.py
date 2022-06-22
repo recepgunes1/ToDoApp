1a956b0c944b2e860344c35fc0d90efd49801bc8
Recep Güneş <recepgunes2@outlook.com>
2022-06-22 17:55:07 +0200

    project layout was changed.

--- app/views.py
+++ app/views.py
@@ -6,42 +6,42 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template('tasks.html')
+    return render_template('task/tasks.html')
 
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('auth/login.html')
 
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('auth/register.html')
 
 
 @app.route('/settings')
 def settings():
-    return render_template('settings.html')
+    return render_template('auth/settings.html')
 
 
 @app.route('/statics')
 def statics():
-    return render_template('statics.html')
+    return render_template('task/statics.html')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('fixed/about.html')
 
 
 @app.route('/create_new_task')
 def create_new_task():
-    return render_template('create_task.html')
+    return render_template('task/create_task.html')
 
 
 @app.route('/get_detail')
 def detail():
-    return render_template('task.html')
+    return render_template('task/task.html')
 
 
 if __name__ == '__main__':
